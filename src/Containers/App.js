763f67a42cf1b429efe548f6a2a08ce611bb4739
Dustin Brickwood <dustinbrickwood204@gmail.com>
2018-02-11 20:36:06 -0500

    commenting

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -17,6 +17,7 @@ class App extends React.Component {
             detailsData: [],
             isEmpty: false
         }
+        
         this.dateParserHandler = this.dateParserHandler.bind(this);
     }
 
